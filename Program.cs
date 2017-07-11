502dc881bc078570144eee3d9d326137e62c7eaa
Kristin Caron <krissycaron@gmail.com>
2017-07-11 16:23:21 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace classesEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
